75d21461fb56080b9606979a8b24c0c12eabcccc
TiagoRibeiro <ribeirotgs@gmail.com>
2022-07-26 15:56:57 -0500

    troubleshooting readme2

--- rqtl/rendering_rmarkdown.R
+++
@@ -1,6 +0,0 @@
-# this is script is here just to rename the rmarkdown output file, not sure how to do it in a nicer way
-setwd('Documents/git_repos/ril_epistasis/RILepi/rqtl/')
-rmarkdown::render("README.Rmd", output_file = "README.html")
-
-
-
